9ba6a7824bf9e6d58a2a05266213465a20960bac
mdsajjad197 <sajjadsaffan634@gmail.com>
2026-02-06 01:04:52 +0530

    axois change

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -7,12 +7,12 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-learn-6cdc.vercel.app',
         changeOrigin: true,
         secure: false,
       },
       '/uploads': {
-        target: 'http://localhost:5000',
+        target: 'https://ai-learn-6cdc.vercel.app',
         changeOrigin: true,
         secure: false,
       },
